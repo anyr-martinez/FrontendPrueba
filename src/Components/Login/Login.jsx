012e01609a07732cccbca83e44ef39f83127f2ba
anyr-martinez <sanchezany2003@gmail.com>
2025-04-01 09:25:44 -0600

    01/04/2025 Logo Actualizado

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useContext } from "react";
 import { useNavigate } from "react-router-dom";
-import compu from "../../assets/images/compu.png";
+import logo from "../../assets/images/logo.png";
 import fondo from "../../assets/images/fondo.jpg";
 import { mostrarAlerta, mostrarAlertaOK, mostrarAlertaError } from "../SweetAlert/SweetAlert";
 import { UsuarioIniciarSesion } from "../Configuration/ApiUrls";
@@ -81,82 +81,76 @@ const Login = () => {
   };
 
   return (
-    <section className="vh-100" style={{ backgroundImage: `url(${fondo})`, backgroundSize: 'contain', backgroundPosition: 'center' }}>
-      <div className="container py-5 h-100">
-        <div className="row d-flex justify-content-center align-items-center h-100">
-          <div className="col col-xl-8">
-            <div className="card" style={{ borderRadius: '2rem', opacity: 0.90 }}> {/* Más transparencia en el rectángulo */}
-              <div className="row g-0">
-                {/* Columna con el formulario de login */}
-                <div className="col-md-6 col-lg-7 d-flex align-items-center justify-content-center">
-                  <div className="card-body p-4 p-lg-5 text-black">
-                    <form onSubmit={handleSubmit}> {/* Conexión con el submit */}
-                      <div className="d-flex align-items-center mb-3 pb-1">
-                        <i className="fas fa-cog fa-2x me-3" style={{ color: '#007236' }}></i>
-                        <span className="h1 fw-bold mb-0">Iniciar Sesión</span>
-                      </div>
-  
-                      {/* Campo de Usuario */}
-                      <div className="form-outline mb-4">
-                        <input
-                          type="text"
-                          id="usuario"
-                          className="form-control form-control-lg"
-                          placeholder="Usuario"
-                          value={usuario}  // Vínculo con el estado
-                          onChange={handleChange}  // Llamada a la función handleChange
-                        />
-                      </div>
-  
-                      {/* Campo de Contraseña */}
-                      <div className="form-outline mb-4">
-                        <input
-                          type="password"
-                          id="password"
-                          className="form-control form-control-lg"
-                          placeholder="Contraseña"
-                          value={contrasena}  // Vínculo con el estado
-                          onChange={handleChange}  // Llamada a la función handleChange
-                        />
-                      </div>
-  
-                      {/* Botón de Iniciar sesión */}
-                      <div className="pt-1 mb-4" style={{ display: 'flex', justifyContent: 'center' }}> {/* Centrado del botón */}
-                        <button
-                          className="btn btn-primary"
-                          type="submit"  // Cambié el tipo a submit
-                          style={{ fontSize: '16px', padding: '10px 50px', width: 'auto' }} // Botón más pequeño y menos largo
-                        >
-                          INGRESAR
-                        </button>
-                      </div>
-                    </form>
-                  </div>
-                </div>
-  
-                {/* Columna con la imagen más pequeña */}
-                <div className="col-md-6 col-lg-5 d-none d-md-block">
-                  <img
-                    src={compu} // Usamos la imagen importada
-                    alt="Login image"
-                    className="img-fluid"
-                    style={{
-                      objectFit: 'cover', // Asegura que la imagen cubra bien el área
-                      objectPosition: 'center', // Centra la imagen
-                      borderRadius: '1rem 0 0 1rem', // Bordes redondeados para la imagen
-                      height: '80%', // Reduce el tamaño de la imagen
-                      width: '90%', // Reduce el tamaño de la imagen
-                      margin: 'auto' // Centra la imagen
-                    }}
-                  />
-                </div>
+    <section className="vh-100" style={{ 
+      backgroundImage: `url(${fondo})`, 
+      backgroundSize: 'contain',  
+      backgroundPosition: 'center', 
+      backgroundRepeat: 'repeat',
+      backgroundColor: 'rgba(0, 0, 0, 2.5)' 
+    }}>
+      <div className="container py-5 h-100 d-flex justify-content-center align-items-center">
+        <div className="col col-xl-6">
+          <div className="card" style={{ 
+            borderRadius: '2rem', 
+            backgroundColor: 'rgba(255, 255, 255, 0.85)', 
+            padding: '30px', 
+            boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.3)'
+          }}>
+            <div className="card-body text-black text-center">
+              {/* Logo */}
+              <div className="mb-3">
+                <img src={logo} alt="Logo" style={{ width: '250px' }} />
               </div>
+              
+              <div className="d-flex align-items-center mb-3 pb-1 justify-content-center">
+                <i className="fas fa-cog fa-2x me-3" style={{ color: '#007236' }}></i>
+                <span className="h2 fw-bold mb-0">Iniciar Sesión</span>
+              </div>
+  
+              <form onSubmit={handleSubmit}>
+                {/* Campo de Usuario */}
+                <div className="form-outline mb-4 position-relative">
+                  <input
+                    type="text"
+                    id="usuario"
+                    className="form-control form-control-lg pe-4"
+                    placeholder="Usuario"
+                    value={usuario}
+                    onChange={handleChange}
+                  />
+                  <i className="fas fa-user position-absolute" style={{ top: '50%', right: '10px', transform: 'translateY(-50%)', color: '#007236' }}></i>
+                </div>
+  
+                {/* Campo de Contraseña */}
+                <div className="form-outline mb-4 position-relative">
+                  <input
+                    type="password"
+                    id="password"
+                    className="form-control form-control-lg pe-4"
+                    placeholder="Contraseña"
+                    value={contrasena}
+                    onChange={handleChange}
+                  />
+                  <i className="fas fa-lock position-absolute" style={{ top: '50%', right: '10px', transform: 'translateY(-50%)', color: '#007236' }}></i>
+                </div>
+  
+                {/* Botón de Iniciar sesión */}
+                <div className="pt-1 mb-4 d-flex justify-content-center">
+                  <button
+                    className="btn btn-primary"
+                    type="submit"
+                    style={{ fontSize: '16px', padding: '10px 50px', width: 'auto' }}
+                  >
+                    INGRESAR
+                  </button>
+                </div>
+              </form>
             </div>
           </div>
         </div>
       </div>
     </section>
-  );
+  ); 
 };
 
 export default Login;
